49da2d8b06ae42707dee0f1cd66e81a3f464a2ff
jioo <rlawl3383@gmail.com>
2025-01-26 14:58:49 +0900

    기본셋팅 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  sassOptions: {
+    reactStrictMode: false,
+    includePaths: ["asset"], // styles 폴더를 SCSS 파일의 경로로 설정
+    prependData: `@import "@/asset/_mixin.scss";`, // _mixin.scss 파일의 경로를 정확히 설정
+  },
+};
 
 export default nextConfig;
