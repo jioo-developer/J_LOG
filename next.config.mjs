34c6f3a650162d92beea03ff6ac4f5f4191838f6
jioo <rlawl3383@gmail.com>
2025-01-28 16:09:38 +0900

    package.json 수정

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,6 @@ const nextConfig = {
   sassOptions: {
     reactStrictMode: false,
     includePaths: ["asset"], // styles 폴더를 SCSS 파일의 경로로 설정
-    prependData: `@import "@/asset/_mixin.scss";`, // _mixin.scss 파일의 경로를 정확히 설정
   },
 };
 
