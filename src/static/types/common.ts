c8872c4ec4d46457bef7a6e21db92b2bea459e21
jioo <rlawl3383@gmail.com>
2025-02-23 17:35:51 +0900

    static type / 상수 추가

---
+++ src/static/types/common.ts
@@ -0,0 +1,32 @@
+import { Timestamp } from "firebase/firestore";
+
+export type InputTypes = {
+  emailRequired: string;
+  passwordRequired: string;
+};
+
+export type AuthPropsType = {
+  email: string;
+  password: string;
+  nickname: string;
+};
+
+export type FirebaseData = {
+  user: string;
+  pageId: string;
+  profile: string;
+  date: string;
+  timestamp: Timestamp;
+  title: string;
+  fileName: string[];
+  url: string[];
+  favorite: number;
+  text: string;
+  writer: string;
+  priority?: boolean;
+  replyLength?: number;
+};
+
+export type Story<T> = {
+  args: Partial<T>;
+};
